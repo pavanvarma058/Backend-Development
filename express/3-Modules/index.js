d7d983fe2d159b269ce784d06787b5fb492554da
Pavan Varma <pavanvarma058@gmail.com>
2025-12-10 23:51:45 +0000

    Learned about modules in express

---
+++ express/3-Modules/index.js
@@ -0,0 +1,6 @@
+const value = require("./math");
+// or
+const { add, sub, mul, div } = require("./math");
+
+// console.log(value.add(2, 9));
+console.log(add(2, 8));
---
+++ express/3-Modules/math.js
@@ -0,0 +1,17 @@
+exports.add = function add(a, b) {
+  return a + b;
+};
+
+exports.sub = function sub(a, b) {
+  return a - b;
+};
+
+exports.mul = function mul(a, b) {
+  return a * b;
+};
+exports.div = function div(a, b) {
+  return a / b;
+};
+
+// 1.Names Exports
+// 2.Default Exports
